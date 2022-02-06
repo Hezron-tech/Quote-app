a5e9cda9e69f528d89ec6adfb952ddb5b7d27c1b
Hezron-tech <kiprophez@gmail.com>
2022-02-06 22:38:14 +0300

    adding new quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -29,6 +29,13 @@ export class QuoteComponent implements OnInit {
   }
 
 
+  addNewQuote(Quote:any){
+    let goalLength = this.quotes.length;
+    Quote.id = goalLength+1;
+    Quote.completeDate = new Date(Quote.completeDate)
+    this.quotes.push(Quote)
+  }
+
 
 
 
